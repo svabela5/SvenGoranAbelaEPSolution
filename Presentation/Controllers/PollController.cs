040e9bd43fe219bf172d82a0e86adec1cf02d9f8
svabela5 <109699629+svabela5@users.noreply.github.com>
2025-04-04 17:35:21 +0200

    Created the add functionality and ordered the  list in descending order

--- Presentation/Controllers/PollController.cs
+++ Presentation/Controllers/PollController.cs
@@ -1,4 +1,5 @@
 ﻿using DataAccess.Repositories;
+using Domain.Models;
 using Microsoft.AspNetCore.Mvc;
 
 namespace Presentation.Controllers
@@ -8,7 +9,30 @@ namespace Presentation.Controllers
         public IActionResult Index([FromServices] PollRepository _pollRepo)
         {
             var polls = _pollRepo.GetPolls();
-            return View(polls);
+            var orderedPolls = polls.OrderByDescending(p => p.CreatedAt);
+            return View(orderedPolls);
+        }
+
+        [HttpGet]
+        public IActionResult Create([FromServices] PollRepository _pollRepo)
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public IActionResult Add(Poll p, [FromServices] PollRepository _pollRepo, [FromServices] IWebHostEnvironment host)
+        {
+            if (ModelState.IsValid)
+            {
+                p.Option1VotesCount = 0;
+                p.Option2VotesCount = 0;
+                p.Option3VotesCount = 0;
+                p.CreatedAt = DateTime.Now;
+                _pollRepo.AddPoll(p);
+                TempData["message"] = "Student was added successfully";
+                return RedirectToAction("Index");
+            }
+            return View();
         }
     }
 }
